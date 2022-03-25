28090016ba81fbf4755e495d469059a24820a6f6
xiexuan-star <xiexuan127@foxmail.com>
2022-03-25 23:54:47 +0800

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+function* generator() {
+  let i = 0;
+  try {
+    while (true) {
+      yield ++i;
+    }
+  } catch (e) {
+    console.log('end');
+    yield 'end'
+  }
+}
+
+const it = generator()
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+// console.log(it.return(100 as any));
+// console.log(it.return(200 as any));
+// console.log(it.next());
+console.log(it.throw('error'));
+console.log(it.next());
